e9a266b37a0d454c6d5d35a538a56c93fb63a5ee
Daniel Torres <danieltorresandrade@gmail.com>
2024-04-01 23:33:01 -0300

    Criando código.
    Neste commit eu criei os arquivos e comecei a criar o código em JavaScript.

---
+++ src/index.js
@@ -0,0 +1,76 @@
+//Curso: Lógica de Programação
+//Instrutor: Felipe Silva Aguiar
+//Instituição: DIO.me
+
+/*## Objetivo:
+
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+
+- nome
+- idade
+- tipo (ex: guerreiro, mago, monge, ninja )
+
+além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+- exibir a mensagem: "o {tipo} atacou usando {ataque}")
+- aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+- e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+se mago -> no ataque exibir (usou magia)
+se guerreiro -> no ataque exibir (usou espada)
+se monge -> no ataque exibir (usou artes marciais)
+se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}"
+  ex: mago atacou usando magia,  guerreiro atacou usando espada
+  */
+
+// Iniciando código
+
+//Criando classe chamada "caracteristicaHeroi"
+class caracteristicasHeroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    escreverSaida(){
+        if(ataqueHeroi === "classe incorreta"){
+            console.log(`Você não escolheu uma classe ou ela está incorreta, favor escolher 
+            entre as classes: guerreiro, mago, monge ou ninja. ` + this.tipo)
+        }else{
+            console.log(`O ${this.nome} atacou usando ${ataqueHeroi}`)
+        }
+    }
+}
+
+let ataqueHeroi = ""
+let heroi = new caracteristicasHeroi("Danielta", "30", "batedor")
+
+switch(heroi.tipo){
+    case "guerreiro":
+        ataqueHeroi = "espada"
+    break
+
+    case "mago":
+        ataqueHeroi = "magia"
+    break
+
+    case "monge":
+        ataqueHeroi = "artes marciais"
+    break
+
+    case "ninja":
+        ataqueHeroi = "shuriken"
+    break
+
+    default:
+        ataqueHeroi = "classe incorreta"
+}
+
+heroi.escreverSaida()
